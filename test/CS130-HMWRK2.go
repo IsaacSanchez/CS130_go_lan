b92fc17a6f80827386a6ad05b03a10c52b02d641
IsaacSanchez <isaacsanchez@mail.fresnostate.edu>
2015-02-09 10:37:33 -0800

    Homework 2
    This are examples of what we learned in class in a program(s): struct
    type, functions, multiple returns, unused returns without error ( _ ),
    constants

---
+++ test/CS130-HMWRK2.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+//THis is the Struct
+
+type Car struct {
+	model string
+	price int
+}
+
+//Some Functions
+
+func printCar(car Car) {
+
+	fmt.Println(car.model, car.price)
+}
+
+func buyCar(car Car) (string, int) {
+	return car.model, car.price
+}
+
+//Some Constants we can use
+const (
+	BASEVAL = 500000
+)
+
+func main() {
+	var car = Car{
+		model: "Ferrari",
+		price: 200000,
+	}
+	printCar(car)
+		var model, _ = buyCar(car)
+			fmt.Println(model)
+
+		fmt.Println(BASEVAL)
+
+}
